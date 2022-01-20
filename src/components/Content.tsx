8cacb1aa492bcfa73a4e57211ce06d509367fcb8
Fatih <lifelonglearnerest@gmail.com>
2022-01-20 13:04:32 +0300

    unused interface

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,12 +1,11 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
 
-interface ContentBodyProps {}
 const ContentBodyContainer = styled.div`
-  background-color: red;
-  justify-content: center;
-`
+    background-color: red;
+    justify-content: center;
+`;
 
-export const Content: React.FC<ContentBodyProps> = ({ children }) => {
-  return <ContentBodyContainer>{children}</ContentBodyContainer>
-}
+export const Content: React.FC = ({ children }) => {
+    return <ContentBodyContainer>{children}</ContentBodyContainer>;
+};
